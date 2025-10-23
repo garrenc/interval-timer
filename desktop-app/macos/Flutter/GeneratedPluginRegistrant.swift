03762ee8611cd270d10d578626f7c36b40222eed
Ruslan Vetoshev <vetoshev.ruslan@gmail.com>
2025-10-24 00:31:30 +0200

    add sound effects after pause/work is ended

--- desktop-app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ desktop-app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import bitsdojo_window_macos
 import flutter_local_notifications
+import path_provider_foundation
 import shared_preferences_foundation
 import system_tray
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SystemTrayPlugin.register(with: registry.registrar(forPlugin: "SystemTrayPlugin"))
 }
